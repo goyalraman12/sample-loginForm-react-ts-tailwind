ec5f63cfa23f8acc5c64f2cd8054785519ea5181
Raman Goyal <ramangoyal@192.168.1.2>
2023-05-14 02:03:38 +0530

    added text center

--- src/components/SuccessAlert.tsx
+++ src/components/SuccessAlert.tsx
@@ -4,7 +4,7 @@ interface SuccessAlertProps {
 const SuccessAlert = ({ msg }: SuccessAlertProps) => {
   return (
     <div
-      className="mt-4 mb-5 mx-auto w-6/12 rounded-lg bg-green-light px-6 py-5 text-green-darker"
+      className="mt-4 mb-5 mx-auto w-6/12 rounded-lg bg-green-light px-6 py-5 text-green-darker text-center"
       role="alert"
     >
       {msg}
